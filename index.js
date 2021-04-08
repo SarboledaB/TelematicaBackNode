a2c2fa3cae29dc95fb6ce0ca2fae76e190213f88
sarboledab <sarboledabotero@gmail.com>
2021-04-08 07:58:18 -0500

    Se soluciona problema de cors

--- index.js
+++ index.js
@@ -20,6 +20,14 @@ app.get('/', (req, res) => {
 // Require car routes
 const carRoutes = require('./src/routes/cars.routes')
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-COntrol-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+}) 
+
 // using as middleware
 app.use('/api/v1/cars', carRoutes)
 
